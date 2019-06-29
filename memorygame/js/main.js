3fe5e66429908b16a9b23c17b89a369ea31eab34
Ninian Bond <recursive.hebrew@gmail.com>
2019-06-29 03:01:39 -0600

    created some game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,9 +1,24 @@
-console.log("Up and running!");
+var cards = ['king', 'king', 'queen', 'queen'];
 
-var cardOne = 'Queen';
-var cardTwo = 'King';
-var cardThree = 'Queen';
-var cardFour = 'King';
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) { 
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+}
